667cb3be301bdeb959d9ce5b56d9c5958b81aaca
sharavak <86647111+sharavak@users.noreply.github.com>
2021-09-20 13:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ const yourIP = async () => {
 
 function update(parsed) {
     if(parsed.reason==='Invalid IP Address'){
-        console.log(parsed);
         alert('Please enter a vaid IP address');
     }
     else{
